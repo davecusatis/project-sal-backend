89ac51def31ca3ba4cb4784502e1094851382326
Dave Cusatis <davecusatis1@gmail.com>
2018-08-13 23:35:32 -0700

    add terraform files

---
+++ terraform/sal.tf
@@ -0,0 +1,15 @@
+provider "aws" {
+  region = "us-west-2"
+}
+
+resource "aws_db_instance" "default" {
+  identifier        = "sal-db"
+  allocated_storage = 20
+  storage_type      = "gp2"
+  engine            = "postgres"
+  engine_version    = "9.6.3"
+  instance_class    = "db.t2.micro"
+  name              = "saldb"
+  username          = "dave"
+  port              = "5432"
+}
