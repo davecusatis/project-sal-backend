894fad526cebe2101397d50af65b024048b0b485
Dave Cusatis <davecusatis1@gmail.com>
2018-10-03 23:46:13 -0700

    add todo with solution so i dont forget

--- project-sal-backend/aggregator/generic_message_sender.go
+++ project-sal-backend/aggregator/generic_message_sender.go
@@ -8,6 +8,10 @@ import (
 	"github.com/davecusatis/project-sal-backend/project-sal-backend/models"
 )
 
+// TODO: make these self cleaning
+// the solution to these dying is like having a one minute ticker that when it recieves a message we return,
+// the ticker should reset when a message is received i think it's doable
+
 // GenericMessageSender is a message GenericMessageSender as to not eat through pubsub rate limits
 type GenericMessageSender struct {
 	MessageChan     chan *models.PubsubMessage
